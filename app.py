15d7b53da7a6afe14cfcbe0936c8c7ddf119e7de
Jakub Hruška <jhruska@mail.muni.cz>
2019-03-05 00:10:03 +0200

    basic layout implemented

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/sample')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
